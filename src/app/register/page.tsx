1cd4034cf49bd09efa42f49f535ed583b76d8e70
Brian Steven <sbrianprog@gmail.com>
2024-11-26 13:43:48 -0500

    fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,7 +18,7 @@ const RegisterUserSchema = Yup.object().shape({
     email: Yup.string().email("Invalid email").required("Email is required"),
     password: Yup.string().required("Password is required"),
     confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], "Passwords must match")
+        .oneOf([Yup.ref('password'), undefined], "Passwords must match")
         .required("Confirm password is required"),
 });
 
